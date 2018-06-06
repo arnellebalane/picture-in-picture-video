00b3ebf389cd2288a7b130e826e37413fff778b0
Arnelle Balane <arnellebalane@gmail.com>
2018-06-06 08:10:38 +0800

    Implement js functionality

---
+++ index.js
@@ -0,0 +1,27 @@
+const video = document.querySelector('video');
+const button = document.querySelector('button');
+
+if (!document.pictureInPictureEnabled) {
+    button.disabled = true;
+    button.textContent = 'PiP is not supported in your browser';
+}
+if (video.disablePictureInPicture) {
+    button.disabled = true;
+    button.textContent = 'PiP is currently disabled';
+}
+
+button.addEventListener('click', () => {
+    if (document.pictureInPictureElement) {
+        document.exitPictureInPicture().catch(console.error);
+    } else {
+        video.requestPictureInPicture().catch(console.error);
+    }
+});
+
+video.addEventListener('enterpictureinpicture', () => {
+    button.textContent = 'Exit Picture-in-Picture';
+});
+
+video.addEventListener('exitpictureinpicture', () => {
+    button.textContent = 'Enter Picture-in-Picture';
+});
